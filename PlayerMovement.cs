58c2b13b357b479034e675782b8b7e287902f56f
conknee000 <114608218+conknee000@users.noreply.github.com>
2022-10-01 13:52:10 -0400

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public int health;
+
+
+    public GameObject[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     Vector2 movement;
     public Camera cam;
@@ -29,5 +36,37 @@ public class PlayerMovement : MonoBehaviour
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 
         rb.rotation = angle; 
+    }
+
+    public void TakeDamage(int amount)
+    {
+        
+        health -= amount;
+        UpdateHealthUI(health);
+       
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+            
+        }
+    }
+
+    void UpdateHealthUI(int currentHealth)
+    {
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if (i < currentHealth)
+            {
+                hearts[i].GetComponent<Image>().sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].GetComponent<Image>().sprite = emptyHeart;
+            }
+
+        }
+
     }
 }
